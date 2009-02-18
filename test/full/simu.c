e6aee33ca29f9530c35131460230c2817ca06d53
barre <devnull@localhost>
2009-02-18 13:20:01 +0000

    Changed to a draft of disassemblier

--- test/full/simu.c
+++ test/full/simu.c
@@ -2,17 +2,57 @@
 #include <ppc/api.h>
 #include <../src/loader.h>
 
+
+
+
+
 int main(int argc, char **argv) {
 	ppc_platform_t *pf;
 	ppc_sym_t s_it;
-	Elf32_Sym *s;
+	Elf32_Shdr *s;
+	Elf32_Shdr *s_tab;
+	int nb_sect_disasm = 0;
+	ppc_loader_t *loader;
+	int i;
+
 	
 	/* test arguments */
 	if(argc != 2) {
 		fprintf(stderr, "ERROR: one argument required: the simulated program !\n");
-		return 2;
+		return 1;
 	}
 	
+	/* we need a loader alone for sections */
+	loader = ppc_loader_open(argv[1]);
+	if (loader == NULL)
+	{
+		fprintf(stderr, "ERROR: cannot load the given executable : %s.", argv[1]);
+		return 2;
+	}
+
+	printf("found %d sections in the executable %s\n", ppc_loader_count_sects(loader), argv[1]);
+	s_tab = malloc(ppc_loader_count_sects(loader) * sizeof(Elf32_Shdr));
+	
+	s = ppc_loader_first_sect(loader, &s_it);
+	printf("\ttype:%08X\tflag:%08X\taddr:%08X\tsize:%08X\n", s->sh_type, s->sh_flags, s->sh_addr, s->sh_size);
+	while (s_it >= 0)
+	{
+		s = ppc_loader_next_sect(loader, &s_it);
+		if (s)
+		{
+			/* if exec section, keep it to disasemble it */
+			if ((s->sh_type == SHT_PROGBITS) && (s->sh_flags == (SHF_ALLOC | SHF_EXECINSTR)))
+			{
+				s_tab[nb_sect_disasm++] = *s;
+				printf("[X]");
+			}
+			printf("\ttype:%08X\tflag:%08X\taddr:%08X\tsize:%08X\n", s->sh_type, s->sh_flags, s->sh_addr, s->sh_size);
+		}
+	}
+printf("found %d section to disasemble\n", nb_sect_disasm);
+
+	ppc_loader_close(loader);
+
 	/* create the platform */
 	pf = ppc_new_platform();
 	if(pf == NULL) {
@@ -21,47 +61,41 @@ int main(int argc, char **argv) {
 	}
 	
 	/* load it */
-	if(ppc_load_platform(pf, argv[1]) == -1) {
+	if(ppc_load_platform(pf, argv[1]) == -1) { /* SIGSEGV ici */
 		fprintf(stderr, "ERROR: cannot load %s\n", argv[1]);
 		return 3;
 	}
 	
-	/*ppc_fetch_t *f = ppc_new_fetch(pf);*/
-	/* the exe i have tested have their code beginnin at this address */
-	/*ppc_address_t a = 0x10000080;
-	int i;
-	for (i=0; i<100; i++)
-	{
-		int n = ppc_fetch(f, a);
-		uint32_t code = ppc_mem_read32(ppc_get_memory(pf, 0), a);
-		//printf("@ %08X => %08X => %d\t\t%s.\n", a, code, n, ppc_get_string_ident(n));
-		a += 4;
-	}
-	ppc_delete_fetch(f);
-	
-	printf("\ntesting decode\n");
+	/* CAUTION: C99 valid declarations, BUT C89 invalid */
+	int i_sect;
 	ppc_decoder_t *d = ppc_new_decoder(pf);
-	a = 0x10000080;
-	for (i=0; i<100; i++)
+	
+	for (i_sect = 0; i_sect<nb_sect_disasm; i_sect++)
 	{
-		char buff[100];
-		ppc_inst_t *inst = ppc_decode(d, a);
-		ppc_disasm(buff, inst);
-		uint32_t code = ppc_mem_read32(ppc_get_memory(pf, 0), a);
-		printf("@ %08X => %08X => %s.\n", a, code, buff);
-		a += 4;
-	}
-	ppc_delete_decoder(d);*/
-	
-	/* dump symbol table */
+		ppc_address_t adr = s_tab[i_sect].sh_addr;
+		/* here we assume an instr is always 4 bytes (work only with 4 byte RISC ISAs) */
+		uint32_t nb_instr = s_tab[i_sect].sh_size / 4;
+		uint32_t size_instr = 4;
+		
+		printf("\ndisasm new section, addr=%08X, size=%08X\n", s_tab[i_sect].sh_addr, s_tab[i_sect].sh_size);
 
-	printf("dump symbol table\n");
-	s = gliss_loader_first_sym()
+		for (i=0; i<nb_instr; i++)
+		{
+			char buff[100];
+			ppc_inst_t *inst = ppc_decode(d, adr);
+			ppc_disasm(buff, inst);
+			uint32_t code = ppc_mem_read32(ppc_get_memory(pf, 0), adr);
+			printf("\t@ %08X => %08X => %s.\n", adr, code, buff);
+			adr += size_instr;
+		}
+	}
 	
-	
+	ppc_delete_decoder(d);
+
 	/* delete the platform */
 	ppc_unlock_platform(pf);
 	fprintf(stderr, "SUCCESS !\n");
+
 	return 0;
 }
 
