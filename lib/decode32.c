b5cb343c9f0e807bd3415db931d74d23fbe46020
casse <devnull@localhost>
2009-09-15 10:25:20 +0000

    Added address in decode function.

--- lib/decode32.c
+++ lib/decode32.c
@@ -91,10 +91,10 @@ gliss_inst_t *gliss_decode(gliss_decoder_t *decoder, gliss_address_t address)
 	///* revert bytes if endianness of host and target are not equals */
 	//if (HOST_ENDIANNESS != TARGET_ENDIANNESS)
 	//	code = ((code&0x0FF)<<24)|((code&0x0FF00)<<8)|((code&0x0FF0000)>>8)|((code&0xFF000000)>>24);
-	
+
 	/* then decode it */
-	
-	return gliss_decode_table[id](code);
+
+	return gliss_decode_table[id](address, code);
 }
 
 
