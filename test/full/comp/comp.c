a1c08e8db8e97e927ab217abc2cae75a416b780d
casse <devnull@localhost>
2009-09-16 07:27:03 +0000

    Added support for stack.

--- test/full/comp/comp.c
+++ test/full/comp/comp.c
@@ -10,7 +10,7 @@ extern char **environ;
 
 /*** GLISS2 simulator ***/
 ppc_sim_t *sim = 0;
-ppc_state_t *state = 0;
+ppc_state_t *state = 0, *save_state = 0;
 ppc_platform_t *platform = 0;
 ppc_loader_t *loader = 0;
 
@@ -20,6 +20,7 @@ int gliss2_prepare(int argc, char **argv) {
 	char *c_ptr = 0;
 	Elf32_Sym *Elf_sym = 0;
 	ppc_sym_t sym_exit = 0, sym_start = 0;
+	int i;
 
 	/* open the exec file */
 	loader = ppc_loader_open(argv[1]);
@@ -122,6 +123,7 @@ int gliss2_prepare(int argc, char **argv) {
 	ppc_stack_fill_env(loader, ppc_get_memory(platform, PPC_MAIN_MEMORY), &env);
 	ppc_registers_fill_env(&env, state);
 	ppc_loader_close(loader);
+
 	return 0;
 }
 
@@ -129,11 +131,40 @@ void gliss2_cleanup(void) {
 	ppc_delete_sim(sim);
 }
 
+void gliss2_step(void) {
+	int i;
+
+	/* save the current state */
+	if(save_state)
+		ppc_delete_state(save_state);
+	save_state = ppc_copy_state(state);
+
+	/* simulate */
+	ppc_step(sim);
+}
+
+int gliss2_ended(void) {
+	return ppc_is_sim_ended(sim);
+}
+
+
+ppc_address_t gliss2_pc(void) {
+	return state->NIA;
+}
+
+int equals2(int param, int i) {
+	switch(param) {
+	case PPC_GPR_T: return state->GPR[i] == save_state->GPR[i];
+	default: return 0;
+	}
+}
+
 
 /*** GLISS1 simulator ***/
-state_t *real_state;
+state_t s1;
+state_t *real_state = 0, *save_real_state = &s1;
 
-void prepare_gliss1(int argc, char **argv, char **envp) {
+int gliss1_prepare(int argc, char **argv, char **envp) {
     code_t buff_instr[20];
     long int instruction_number;
     void *system_list[3];
@@ -170,27 +201,174 @@ void prepare_gliss1(int argc, char **argv, char **envp) {
 
     /* Init Emulator */
     real_state=iss_init(mem_list,loader_list,system_list,NULL,NULL);
+    return real_state == 0;
+}
+
+void gliss1_cleanup(void) {
+}
+
+void gliss1_step(void) {
+    code_t buff_instr[20];
+    instruction_t *i;
+
+	/* save state */
+	memcpy(save_real_state, real_state, sizeof(state_t));
+	/*iss_dump(stdout, real_state);*/
+
+	/* emulation */
+	iss_fetch(NIA(real_state), buff_instr);
+	i = iss_decode(real_state, NIA(real_state), buff_instr);
+	iss_complete(i, real_state);
+	iss_free(i);
+}
+
+int gliss1_ended(void) {
+	return !running;
+}
+
+ppc_address_t gliss1_pc(void) {
+	return NIA(real_state);
+}
+
+int equals1(int param, int i) {
+	switch(param) {
+	case GPR_T: return real_state->gpr[i] == save_real_state->gpr[i];
+	default: return 0;
+	}
 }
 
 
+/*** stack compare ***/
+char display(char c) {
+	if(isprint(c))
+		return c;
+	else
+		return '?';
+}
+
+void compare_stack(void) {
+	address_t addr1 = real_state->gpr[1], top1 = 0x80000000;
+	ppc_address_t addr2 = state->GPR[1], top2 = 0x80000000;
+	printf("STACK COMPARISON\n");
+
+	while(addr1 < top1 || addr2 < top2) {
+
+		// state 1
+		if(addr1 < top1) {
+			unsigned char	b0 = iss_mem_read8_little(real_state->M, addr1),
+					b1 = iss_mem_read8_little(real_state->M, addr1 + 1),
+					b2 = iss_mem_read8_little(real_state->M, addr1 + 2),
+					b3 = iss_mem_read8_little(real_state->M, addr1 + 3);
+			printf("%08lx %c%c%c%c %02x%02x%02x%02x\t",
+				(unsigned long)addr1, display(b0), display(b1), display(b2), display(b3), b0, b1, b2, b3);
+		}
+		else
+			printf("%08lx ???? XXXXXXXX\t", (unsigned long)addr1);
+		addr1 += 4;
+
+		// state 2
+		if(addr2 < top2) {
+			unsigned char	b0 = ppc_mem_read8(ppc_get_memory(platform, PPC_MAIN_MEMORY), addr2 + 0),
+					b1 = ppc_mem_read8(ppc_get_memory(platform, PPC_MAIN_MEMORY), addr2 + 1),
+					b2 = ppc_mem_read8(ppc_get_memory(platform, PPC_MAIN_MEMORY), addr2 + 2),
+					b3 = ppc_mem_read8(ppc_get_memory(platform, PPC_MAIN_MEMORY), addr2 + 3);
+			printf("%08lx %c%c%c%c %02x%02x%02x%02x\n",
+				(unsigned long)addr2, display(b0), display(b1), display(b2), display(b3), b0, b1, b2, b3);
+		}
+		else
+			printf("%08x ???? XXXXXXXX\t", addr2);
+		addr2 += 4;
+	}
+}
+
+void display_states(const char *msg, state_t *state1, ppc_state_t *state2) {
+	int i;
+	printf("%s\tSTATE 1\t STATE2\n", msg);
+	for(i = 0; i < 32; i++)
+		printf("r%d\t%08x %08x\n", i, state1->gpr[i], state2->GPR[i]);
+}
+
 /*** main program ***/
-int main(int argc, char **argv)
-{
-	int res;
+int main(int argc, char **argv) {
+	int res, i;
 
 	/* test argument count */
 	assert(argc >= 2);
 
 	/* preparation */
+	res = gliss1_prepare(argc, argv, environ);
+	if(res != 0)
+		return res;
 	res = gliss2_prepare(argc, argv);
 	if(res != 0)
 		return res;
 
+	compare_stack();
+
 	/* perform the simulation */
-	while(!ppc_is_sim_ended(sim))
-		ppc_step(sim);
+	while(1) {
+
+		// test PC equality
+		{
+			ppc_address_t pc1 = gliss1_pc();
+			ppc_address_t pc2 = gliss2_pc();
+			if(pc1 != pc2) {
+				fprintf(stderr, "ERROR: pc diverge (pc1 = %08x, pc2 = %08x)\n", pc1, pc2);
+				return 1;
+			}
+		}
+
+		// test end
+		{
+			int ended2 = gliss2_ended();
+			int ended1 = gliss1_ended();
+			if(ended1 != ended2) {
+				fprintf(stderr, "ERROR: different ended result (1 -> %d, 2 -> %d)\n", ended1, ended2);
+				return 1;
+			}
+			else if(ended1) {
+				fprintf(stderr, "SUCCESS: co-simulation ended.\n");
+				break;
+			}
+		}
+
+		// simulation step
+		printf("=== GLISS1 ===\n");
+		gliss1_step();
+		printf("=== GLISS2 ===\n");
+		gliss2_step();
+		printf("\n\n");
+
+		// state comparison
+		for(i = 0; i < 32; i++) {
+			int eq1 = equals1(GPR_T, i);
+			int eq2 = equals2(PPC_GPR_T, i);
+			if(eq1 != eq2) {
+				fprintf(stdout, "ERROR: difference in change for r%d (1: ", i);
+				if(eq1)
+					fprintf(stdout, "%08x", real_state->gpr[i]);
+				else
+					fprintf(stdout, "%08x -> %08x", save_real_state->gpr[i], real_state->gpr[i]);
+				fprintf(stderr, ", ");
+				if(eq2)
+					fprintf(stdout, "%08x", state->GPR[i]);
+				else
+					fprintf(stdout, "%08x -> %08x", save_state->GPR[i], state->GPR[i]);
+				fprintf(stdout, ")\n");
+				display_states("current", real_state, state);
+				return 1;
+			}
+			if((!eq1 || !eq2) && state->GPR[i] != real_state->gpr[i]) {
+				display_states("before", save_real_state, save_state);
+				fprintf(stdout, "ERROR: difference in value for r%d: 1->%08x, 2->%08x\n",
+					i, real_state->gpr[i], state->GPR[i]);
+				return 1;
+			}
+		}
+	}
 
 	/* cleanup */
+	gliss1_cleanup();
 	gliss2_cleanup();
 	return 0;
 }
