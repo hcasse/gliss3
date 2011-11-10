b7f6fa628f101e840522f42ee3750e75c565e090
Hugues Cassé <casse@irit.fr>
2011-11-10 14:44:29 +0100

    Added support for dynamic-library compiling (for Linux 64).

--- gep/gliss_attr.ml
+++ gep/gliss_attr.ml
@@ -33,6 +33,7 @@ let out = ref "attr.c"
 let template = ref ""
 let do_func = ref false
 let do_proc = ref false
+let do_copy = ref false
 let attr = ref ""
 let def = ref "0"
 let extends: string list ref = ref []
@@ -43,7 +44,8 @@ let options = [
 	("-f", Arg.Set do_func, "generate functions from expression");
 	("-d", Arg.Set_string def, "default value");
 	("-e", Arg.String (fun arg -> extends := arg::!extends), "extension files");
-	("-p", Arg.Set do_proc, "generate functions from action attribute")
+	("-p", Arg.Set do_proc, "generate functions from action attribute");
+	("-c", Arg.Set do_copy, "generate the content of a string as-is (without quotes)");
 ]
 
 
@@ -58,7 +60,7 @@ let process inst out info =
 	let process e =
 
 		(* constant attribute *)
-		if not !do_func then
+		if not !do_func  then
 			Irg.output_const info.Toc.out (Sem.eval_const e)
 
 		(* function attribute *)
@@ -86,20 +88,34 @@ let process inst out info =
 			Toc.cleanup_temps info;
 			Irg.param_unstack params
 		end in
-
+	
+	let rec process_copy e =
+		match e with
+		| Irg.ELINE(_, _, e) ->
+			process_copy e
+		| Irg.CONST(_, Irg.STRING_CONST(v, _, _)) ->
+			output_string info.Toc.out v
+		| _ -> raise (Toc.Error (Printf.sprintf "attribute %s must be an action !" !attr)) in
+	
 	try
 		(* process a procedure *)
 		if !do_proc then
 			match Iter.get_attr inst !attr with
 			| Iter.EXPR _ -> raise (Toc.Error (Printf.sprintf "attribute %s must be an action !" !attr))
-			| Iter.STAT s ->
-				Toc.gen_action info !attr
+			| Iter.STAT s -> Toc.gen_action info !attr
+
+		(* just copy *)
+		else if !do_copy then
+			match Iter.get_attr inst !attr with
+			| Iter.EXPR e -> process_copy e
+			| _ -> raise (Toc.Error (Printf.sprintf "attribute %s must be a string constant !" !attr))
 
 		(* process a function *)
 		else
 			match Iter.get_attr inst !attr with
 			| Iter.EXPR e -> process e
 			| Iter.STAT _ -> raise (Toc.Error (Printf.sprintf "attribute %s must be an expression !" !attr))
+		
 	with Not_found ->
 		output_string info.Toc.out !def
 	|	Sem.SemError msg ->
